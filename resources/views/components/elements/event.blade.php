3877a946409372c110f6a372768c76519669ac19
dipeshmurmu2005 <dipeshmurmu7@gmail.com>
2025-09-16 00:20:21 +0545

    adds event cards

---
+++ resources/views/components/elements/event.blade.php
@@ -0,0 +1,27 @@
+<div class="rounded-xl bg-gradient-to-t from-orange-50 to-white">
+    <div class="rounded-xl overflow-hidden relative">
+        <img src="https://www.eventbookings.com/wp-content/uploads/2018/03/event-ideas-for-party-eventbookings.jpg"
+            alt="">
+        <div class="py-5 absolute left-0 top-0 bg-[rgba(0,0,0,0.6)] h-full w-full p-5 flex items-center">
+            <h2 class="font-bold text-2xl text-white">Rythms Live</h2>
+        </div>
+    </div>
+    <div class="p-2">
+        <div class="flex p-5 divide-x divide-gray-300">
+            <div class="pr-5 text-2xl">
+                <h3>DEC</h3>
+                <h2 class="font-bold text-4xl">24</h2>
+            </div>
+            <div class="pl-5 space-y-2">
+                <div class="flex items-center text-gray-700 text-xs">
+                    <x-heroicon-o-map-pin class="h-4 w-4" />
+                    <h2>Birtamode</h2>
+                </div>
+                <p>Going hard this December with your favourite artists</p>
+                <div class="flex justify-end mt-5">
+                    <button class="btn btn-primary rounded-full btn-sm">Get Ticket</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/livewire/home-wire.blade.php
+++ resources/views/livewire/home-wire.blade.php
@@ -1,4 +1,20 @@
 <div>
     <x-home.banner />
     <x-home.categories />
+    <div class="2xl:px-46 pb-20">
+        <div class="mb-10 flex gap-5">
+            <h2 class="font-semibold w-fit border-b-3 border-primary text-primary pb-2 cursor-pointer ">All</h2>
+            <h2 class="font-semibold w-fit border-b-3 border-transparent cursor-pointer pb-2">Today</h2>
+        </div>
+
+        <div class="grid grid-cols-4 gap-10">
+            <x-elements.event />
+            <x-elements.event />
+            <x-elements.event />
+            <x-elements.event />
+        </div>
+        <div class="mt-10 flex justify-center">
+            <button class="btn btn-primary btn-outline rounded-full">Load More</button>
+        </div>
+    </div>
 </div>
