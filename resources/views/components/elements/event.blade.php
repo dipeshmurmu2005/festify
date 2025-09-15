eb76abb9da92fee926ddcc7868dcdf7a70107070
dipeshmurmu2005 <dipeshmurmu7@gmail.com>
2025-09-16 00:47:55 +0545

    changes the ui of event card

--- resources/views/components/elements/event.blade.php
+++ resources/views/components/elements/event.blade.php
@@ -1,27 +1,41 @@
-<div class="rounded-xl bg-gradient-to-t from-orange-50 to-white">
-    <div class="rounded-xl overflow-hidden relative">
-        <img src="https://www.eventbookings.com/wp-content/uploads/2018/03/event-ideas-for-party-eventbookings.jpg"
-            alt="">
-        <div class="py-5 absolute left-0 top-0 bg-[rgba(0,0,0,0.6)] h-full w-full p-5 flex items-center">
-            <h2 class="font-bold text-2xl text-white">Rythms Live</h2>
-        </div>
-    </div>
-    <div class="p-2">
-        <div class="flex p-5 divide-x divide-gray-300">
-            <div class="pr-5 text-2xl">
-                <h3>DEC</h3>
-                <h2 class="font-bold text-4xl">24</h2>
+<a href="#" class="overflow-hidden flex justify-center items-center relative">
+    <div class="absolute w-full z-10 left-0 top-32">
+        <div class="relative items-center">
+            <div
+                class="h-16 w-16 bg-white rounded-full absolute -left-8 outline-white outline-2 outline-dashed outline-offset-2">
+
             </div>
-            <div class="pl-5 space-y-2">
-                <div class="flex items-center text-gray-700 text-xs">
-                    <x-heroicon-o-map-pin class="h-4 w-4" />
-                    <h2>Birtamode</h2>
-                </div>
-                <p>Going hard this December with your favourite artists</p>
-                <div class="flex justify-end mt-5">
-                    <button class="btn btn-primary rounded-full btn-sm">Get Ticket</button>
-                </div>
+            <div
+                class="h-16 w-16 bg-white rounded-full absolute -right-8 outline-white outline-2 outline-dashed outline-offset-2">
+
             </div>
         </div>
     </div>
-</div>
+    <div class="rounded-xl bg-gradient-to-t from-orange-50 to-white">
+        <div class="rounded-xl overflow-hidden relative">
+            <img src="https://www.eventbookings.com/wp-content/uploads/2018/03/event-ideas-for-party-eventbookings.jpg"
+                alt="">
+            <div class="p-10 absolute left-0 top-0 bg-[rgba(0,0,0,0.6)] h-full w-full flex items-center">
+                <h2 class="font-bold text-2xl text-white">Rythms Live</h2>
+            </div>
+        </div>
+        <div class="p-2">
+            <div class="flex p-5 divide-x divide-gray-300 divide-dashed">
+                <div class="pr-5 text-2xl">
+                    <h3>DEC</h3>
+                    <h2 class="font-bold text-4xl">24</h2>
+                </div>
+                <div class="pl-5 space-y-2">
+                    <div class="flex items-center text-gray-700 text-xs">
+                        <x-heroicon-o-map-pin class="h-4 w-4" />
+                        <h2>Birtamode</h2>
+                    </div>
+                    <p>Going hard this December with your favourite artists</p>
+                    <div class="flex justify-end mt-5">
+                        <button class="btn btn-primary rounded-full btn-sm">Get Ticket</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</a>
